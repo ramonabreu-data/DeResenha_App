e3d1c04c2fe5cbaf7e7ef2a169156048c6f90a2b
ramonabreu-data <cdevabreu@gmail.com>
2023-10-13 22:55:18 -0300

    Título principal

--- de_resenhaApp/App.js
+++ de_resenhaApp/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, title,Text} from 'react-native';
+import { View,title} from 'react-native';
 import BottomBar from './components/BottomBar'; // Importe o componente
 import AppBar from './components/AppBar';
 import Logo from './components/Logo';
+import Nome from './components/Nome';
 
 
 const MainScreen = () => {
@@ -11,10 +12,9 @@ const MainScreen = () => {
       
       <AppBar title="De Resenha SC"/>
       <Logo /> 
-
-      
       {/* Adicione o componente BottomBar no final da tela */}
       <BottomBar />
+      <Nome />
     </View>
   );
 };
